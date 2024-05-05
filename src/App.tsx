51be29b298ea1f073807fb98159d9f731ab280e6
sangcs321 <21130511@st.hcmuaf.edu.vn>
2024-05-05 22:09:48 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header id="header">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12">
+              <div className="header_top">
+                <div className="header_top_left">
+                  <ul className="top_nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="pages/contact.html">Contact</a></li>
+                  </ul>
+                </div>
+                <div className="header_top_right">
+                  <p>Friday, December 05, 2045</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-12 col-md-12 col-sm-12">
+              <div className="header_bottom">
+                <div className="logo_area"><a href="index.html" className="logo"><img src="images/logo.jpg" alt=""/></a>
+                </div>
+                <div className="add_banner"><a href="#"><img src="images/addbanner_728x90_V1.jpg" alt=""/></a></div>
+              </div>
+            </div>
+          </div>
+        </header>
+
+      </div>
   );
 }
 
